83c615e481960e49da6a1979ad5c97fe59646ebd
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-07-22 11:01:41 +0000

    Carta

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("todo esta ok");
 };
+
+/*let numeroRandom = (){
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+  let palos = ['Diamante', 'Pica', 'Trebol', 'Corazon']
+
+  let numero = Math.floor(Math.random() * numbers.length);
+  let palo = Math.floor(Math.random() * palos.length);
+
+}
+  */
