c3f12df2204ddc14256aa40cbbda41ad70038815
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-07-23 09:42:37 +0000

    carta y fondo listo

--- src/app.js
+++ src/app.js
@@ -5,16 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("todo esta ok");
-};
+function cartaAleatoria() {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palos = ["♦", "♥", "♠", "♣"];
 
-/*let numeroRandom = (){
-  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-  let palos = ['Diamante', 'Pica', 'Trebol', 'Corazon']
+  let numeroRandom = numeros[Math.floor(Math.random() * numeros.length)];
+  let paloRandom = palos[Math.floor(Math.random() * palos.length)];
 
-  let numero = Math.floor(Math.random() * numbers.length);
-  let palo = Math.floor(Math.random() * palos.length);
+  const topSuit = document.getElementById("top-suited");
+  const value = document.getElementById("number");
+  const bottomSuit = document.getElementById("bottom-suited");
 
+  topSuit.innerText = paloRandom;
+  value.innerText = numeroRandom;
+  bottomSuit.innerText = paloRandom;
+
+  if (paloRandom === "♦" || paloRandom === "♥") {
+    topSuit.style.color = "red";
+    value.style.color = "red";
+    bottomSuit.style.color = "red";
+  } else {
+    topSuit.style.color = "black";
+    value.style.color = "black";
+    bottomSuit.style.color = "black";
+  }
 }
-  */
+
+window.onload = cartaAleatoria;
